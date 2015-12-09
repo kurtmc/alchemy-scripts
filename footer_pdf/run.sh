01902aa5ee8e92cb3d411c7419e84d5ff03d754d
Kurt McAlpine <kurt@linux.com>
2015-12-10 12:56:23 +1300

    Handling spaces in filenames

--- footer_pdf/run.sh
+++ footer_pdf/run.sh
@@ -7,17 +7,17 @@ rm -r $TMP_DIR
 mkdir -p $TMP_DIR
 
 
-cp $FILENAME $TMP_DIR/
-pdfseparate $TMP_DIR/$FILENAME $TMP_DIR/tmp_%d.pdf
+cp "$FILENAME" "$TMP_DIR/"
+pdfseparate "$TMP_DIR/$FILENAME" "$TMP_DIR/tmp_%d.pdf"
 
 FIRST=$(echo $TMP_DIR/tmp_1.pdf | sed -e 's/[\/&]/\\&/g')
 
 cat footer.tex | sed "s/%%FILENAME%%/$FIRST/" | pdflatex &>/dev/null && rm texput.log texput.aux
 
-rm $TMP_DIR/$FILENAME
+rm "$TMP_DIR/$FILENAME"
 mv texput.pdf $TMP_DIR/tmp_1.pdf
 
-pdfunite $TMP_DIR/tmp_* $TMP_DIR/$FILENAME.new
+pdfunite $TMP_DIR/tmp_* "$TMP_DIR/$FILENAME.new"
 
 NOEXT="${FILENAME%.*}"
 
