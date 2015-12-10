9db191057ef9ecd8b1fe7c3a650da4b77a53562e
Kurt McAlpine <kurt@linux.com>
2015-12-10 16:28:55 +1300

    Formatting

--- get_active_product_list.py
+++ get_active_product_list.py
@@ -10,7 +10,6 @@ nz_active = read_csv(nz_active_file, skip_first=True)
 nz_active = get_columns(nz_active, [0, 1])
 nz_active = add_column(nz_active, "NZ")
 
-
 aus_active = read_csv(aus_active_file, skip_first=True)
 aus_active = get_columns(aus_active, [0, 1])
 aus_active = add_column(aus_active, "AUS")
