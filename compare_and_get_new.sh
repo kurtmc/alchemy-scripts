cea35d22711ca13da78bbe7b7354a188fe9a2082
Kurt McAlpine <kurt@linux.com>
2016-01-07 13:15:32 +1300

    Improved command line

---
+++ compare_and_get_new.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# usage: ./compare_and_get_new.sh old_dir new_dir out_dir
+function useage {
+	echo "usage: ./compare_and_get_new.sh old_dir new_dir out_dir"
+}
+
+OLD_DIR=$1
+NEW_DIR=$2
+OUTPUT=$3
+
+if [ -n "$OLD_DIR" ]; then
+	useage
+fi
+if [ -n "$NEW_DIR" ]; then
+	useage
+fi
+if [ -n "$OUTPUT" ]; then
+	useage
+fi
+
+
+mkdir $OUTPUT
+
+diff --brief $OLD_DIR $NEW_DIR | grep ^"Only in $NEW_DIR" | awk -F': ' '{ print $2 }' | while read f; do
+cp "$NEW_DIR/$f" $OUTPUT
+done
+
+diff --brief $OLD_DIR $NEW_DIR | grep ^Files | sed 's/ differ//' | awk -F' and ' '{ print $2 }' | while read f; do
+cp "$f" $OUTPUT
+done
